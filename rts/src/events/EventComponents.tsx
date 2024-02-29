198d3505823803ddf8acbd74bb4fae47e9ab43e3
한경만 <kmhan@spectra.co.kr>
2024-02-29 15:16:06 +0900

    update

--- rts/src/events/EventComponents.tsx
+++ rts/src/events/EventComponents.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 const EventComponents: React.FC = () => {
-    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        console.log(event.target.value);
-    }
-    const onDragStart = (event:React.DragEvent<HTMLInputElement>) => {
-        console.log(event);
-    };
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+  };
 
-    return <div>
-        <input onChange={onChange} />
-        <div draggable onDragStart={onDragStart}>
-            Drag Me!
-        </div>
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log("im being dragged");
+  };
+  return (
+    <div>
+      <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
-}
+  );
+};
 
 export default EventComponents;
